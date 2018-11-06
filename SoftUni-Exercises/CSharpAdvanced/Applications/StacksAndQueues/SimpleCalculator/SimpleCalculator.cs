5c36fc4b99479214dc1acd25ffa3f7a27689117a
Nikola Siderov <iamnikkysiderov@gmail.com>
2018-11-06 19:36:41 +0300

    added new solved problems

--- SoftUni-Exercises/CSharpAdvanced/Applications/StacksAndQueues/SimpleCalculator/SimpleCalculator.cs
+++ SoftUni-Exercises/CSharpAdvanced/Applications/StacksAndQueues/SimpleCalculator/SimpleCalculator.cs
@@ -10,7 +10,27 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine().Split(' ');
 
+            var stack = new Stack<string>(input.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var firstNum = int.Parse(stack.Pop());
+                var oper = stack.Pop();
+                var secondNum = int.Parse(stack.Pop());
+
+                if (oper == "+")
+                {
+                    stack.Push((firstNum + secondNum).ToString());
+                }
+                else
+                {
+                    stack.Push((firstNum - secondNum).ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ SoftUni-Exercises/CSharpAdvanced/Applications/StacksAndQueuesExercise/SequenceWithQueue/SequenceWithQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceWithQueue
+{
+    class SequenceWithQueue
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var previousNum = n;
+
+            int counter = 1;
+
+            Console.Write(n + " ");
+
+            while (counter <= 50)
+            {
+                if (counter <= 50)
+                {
+                    Console.Write(previousNum + 1 + " ");
+                    counter++;
+                }
+
+                if (counter <= 50)
+                {
+                    Console.Write(2 * previousNum + 1 + " ");
+                    counter++;
+                }
+
+                if (counter <= 50)
+                {
+                    Console.Write(previousNum + 2 + " ");
+                    counter++;
+                }
+
+                previousNum += 1;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
