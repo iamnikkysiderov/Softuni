111ff52ccca6848af72c33440e14a560a56ea83d
Nikola Siderov <iamnikkysiderov@gmail.com>
2019-01-08 16:37:32 +0300

    Solved another OOP problem

--- SoftUni-Exercises/CSharpOOPBasics/Applications/DefiningClasses/PersonClass/Person.cs
+++ SoftUni-Exercises/CSharpOOPBasics/Applications/DefiningClasses/PersonClass/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace PersonClass
 {
@@ -10,5 +11,16 @@ namespace PersonClass
         int age;
         List<BankAccount> accounts;
 
+        public Person(string name, int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = accounts;
+        }
+
+        public decimal GetBalance()
+        {
+            return accounts.Sum(x => x.Balance);
+        }
     }
 }
