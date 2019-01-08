ced2a5afd6c9f4206888a1461c747dd2d46ac2ad
Nikola Siderov <iamnikkysiderov@gmail.com>
2019-01-08 14:20:38 +0300

    Updated OOP Solved Exercises

---
+++ SoftUni-Exercises/CSharpOOPBasics/Applications/DefiningClasses/PersonClass/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonClass
+{
+    class Person
+    {
+        string name;
+        int age;
+        List<BankAccount> accounts;
+
+    }
+}
