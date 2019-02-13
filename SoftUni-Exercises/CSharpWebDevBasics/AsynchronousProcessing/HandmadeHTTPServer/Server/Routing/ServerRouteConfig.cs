a8bd282ab110810b35796d7d02ab5208d093a06c
Nikola Siderov <iamnikkysiderov@gmail.com>
2019-02-13 12:39:25 +0300

    http server continued

--- SoftUni-Exercises/CSharpWebDevBasics/AsynchronousProcessing/HandmadeHTTPServer/Server/Routing/ServerRouteConfig.cs
+++ SoftUni-Exercises/CSharpWebDevBasics/AsynchronousProcessing/HandmadeHTTPServer/Server/Routing/ServerRouteConfig.cs
@@ -29,6 +29,29 @@ namespace HandmadeHTTPServer.Server.Routing
 
         private void InitializeRouteConfig(IAppRouteConfig appRouteConfig)
         {
+            foreach (var registeredRoute in appRouteConfig.Routes)
+            {
+                var requestMethod = registeredRoute.Key;
+                var routesWithHandlers = registeredRoute.Value;
+
+                foreach (var routeWithHandler in routesWithHandlers)
+                {
+                    var route = routeWithHandler.Key;
+                    var handler = routeWithHandler.Value;
+
+                    var parameters = new List<string>();
+
+                    var parsedRouteRegex = this.ParseRoute(route, parameters);
+
+                    var routingContext = new RoutingContext(handler, parameters);
+
+                    this.routes[requestMethod].Add(parsedRouteRegex, routingContext)
+                }
+            }
+        }
+
+        private object ParseRoute(string route, List<string> parameters)
+        {
             throw new NotImplementedException();
         }
     }
